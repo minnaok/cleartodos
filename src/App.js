a9024cc3b1b2ef2a7a16c5fd3c7327b443deff19
minnaok <minnoksa@gmail.com>
2020-12-02 21:48:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useState }  from 'react';
 import './App.css';
 
 function App() {
+  const [todo, setTodo] = useState({desc: '', date: ''});
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo({ desc: '' , date:'' });
+  }
+
+  const clearTodos = (event) => {
+    event.preventDefault();
+    setTodos([]);
+  }
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+
+  } 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <header className="App-header">
+      <h1>My Todolist</h1>
       </header>
+
+     Date: <input type="date" placeholder="Date" name="date" value={todo.date} onChange={inputChanged}/>
+      Description: <input type="text" placeholder="Description" name="desc" value={todo.desc} onChange={inputChanged}/>
+      
+      <button onClick={addTodo}>Add</button>
+      <button onClick={clearTodos}>Clear</button>
+      <table><tbody>
+      {
+      todos.map((todo, index) => 
+        <tr key={index}>
+          <td>{todo.date}</td>
+          <td>{todo.desc}</td>
+        </tr>)
+      }
+      </tbody></table>
     </div>
   );
 }
 
 export default App;
+
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+test('add todo', () => {
+const{ container, getByText, getByPlaceholderText } = render(<App/>);
+const desc= getByPlaceholderText('Description');
+fireEvent.change(desc, { target:{ value:'Go to coffee' } 
+}) 
+const date= getByPlaceholderText('Date');
+fireEvent.change(date, { target:{ value:'29.11.2019' } })
+const button= getByText('Add');fireEvent.click(button);
+expect(container).toHaveTextContent('Go to coffee');
+})
+
+test('clear todos', () => {
+    const{ container, getByText, getByPlaceholderText } = render(<App/>);
+    const desc= getByPlaceholderText('Description');
+    fireEvent.change(desc, { target:{ value:'Go to coffee' } 
+    }) 
+    const date= getByPlaceholderText('Date');
+    fireEvent.change(date, { target:{ value:'29.11.2019' } })
+    const button= getByText('Clear');fireEvent.click(button);
+    expect(container).not.toHaveTextContent('');
+    })
